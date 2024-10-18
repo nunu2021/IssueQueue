5e1aab8e29e423487fae05ce6b01be2719779982
michalj1 <michalj2@illinois.edu>
2024-10-18 12:28:54 -0500

    Begin defining required functions

--- iq.h
+++ iq.h
@@ -8,3 +8,27 @@ typedef struct iq_entry {
     bool rdy1;
     bool rdy2;
 } iq_entry;
+
+class iq {
+private:
+    iq_entry entries[64];
+
+public:
+    /* Constructor: initializes new queue */
+    iq();
+
+    /**
+     * Checks whether the queue is full
+     * 
+     * returns true if no free entries exist in the IQ
+     */
+    bool isfull();
+
+    bool insert();
+
+    void wakeup();
+
+    int issue();
+
+    void flush();
+};
