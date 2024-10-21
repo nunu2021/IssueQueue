27b47fa739ba93070499894efefe127363c6975c
michalj1 <michalj2@illinois.edu>
2024-10-21 18:40:40 -0500

    Implement initial insertion arbitration

--- iq.cpp
+++ iq.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 IQ::IQ() {
-    // Currently iterative, but pretend it's parallel
-    IQ_occupancy = 0;
-
     for (int i = 0; i < NUM_ENTRIES; i++) {
         entries[i].valid = false;
     }
+
+    arbitration_head = initializeArbiters(0, NUM_ENTRIES);
+    arbiter_depth = calculateDepth(arbitration_head);
 }
 
 bool IQ::isfull(){
@@ -19,17 +19,44 @@ bool IQ::isfull(){
     return false;
 }
 
-bool IQ::insert(){
+bool IQ::insert(bool in_rob_idx[7], uint8_t in_src1, uint8_t in_src2,
+            bool in_rdy1, bool in_rdy2){
     if (IQ_occupancy < 64) {
         IQ_occupancy++;
-    // TODO: insert the instruction into the IQ
+        // TODO: insert the instruction into the IQ
+        IQ::arbiter* current = arbitration_head;
+        for (int i = 0; i < arbiter_depth - 1; i++) {
+            for (int j = 0; j < ARBITER_WIDTH; j++) {
+                if (current->children[j]->free > 0) {
+                    current->free--;
+                    current = current->children[j];
+                    break;
+                }
+            }
+        }
+
+        int start = current->start_index;
+        for (int i = 0; i < ARBITER_WIDTH; i++) {
+            if (!entries[start + i].valid) {
+                entries[start + i].valid = true;
+                for (int j = 0; j < 7; j++) {
+                    entries[start + i].rob_index[j] = in_rob_idx[j];
+                }
+                entries[start + i].src1 = in_src1;
+                entries[start + i].src2 = in_src2;
+                entries[start + i].rdy1 = in_rdy1;
+                entries[start + i].rdy2 = in_rdy2;
+
+                current->free--;
+                break;
+            }
+        }
         return true;
     }
     return false;
 }
 
 void IQ::wakeup(uint8_t produced_reg){
-    // Currently iterative, but pretend it's parallel
     for (int i = 0; i < NUM_ENTRIES; i++) {
         if (entries[i].src1 == produced_reg) {
             entries[i].rdy1 = true;
@@ -59,10 +86,39 @@ int IQ::issue(){
 }
 
 void IQ::flush(){
-    // Currently iterative, but pretend it's parallel
     for (int i = 0; i < NUM_ENTRIES; i++) {
         entries[i].valid = false;
     }
     IQ_occupancy = 0;
 }
 
+IQ::arbiter* IQ::initializeArbiters(int start, int end) {
+    IQ::arbiter* arbiter = new IQ::arbiter;
+    arbiter->start_index = start;
+    arbiter->end_index = end;
+    arbiter->free = end - start;
+    arbiter->ready = 0;
+    if (end - start > ARBITER_WIDTH) {
+        int interval = (end - start) / ARBITER_WIDTH; // Assume it divides
+        for (int i = 0; i < ARBITER_WIDTH; i++) {
+            arbiter->children[i] = initializeArbiters(start + i * interval,
+                                                start + (i + 1) * interval);
+        }
+    }else {
+        for (int i = 0; i < ARBITER_WIDTH; i++) {
+            arbiter->children[i] = nullptr;
+        }
+    }
+}
+
+int IQ::calculateDepth(IQ::arbiter* head) {
+    if (head == nullptr) {
+        return 0;
+    }
+    int depth = 1;
+    while (head->children[0] != nullptr) {
+        head = head->children[0];
+        depth++;
+    }
+    return depth;
+}
--- iq.h
+++ iq.h
@@ -7,6 +7,7 @@
 
 #define ISSUE_WIDTH 4
 #define NUM_ENTRIES 64
+#define ARBITER_WIDTH 4
 
 class IQ {
     typedef struct iq_entry {
@@ -18,10 +19,22 @@ class IQ {
         bool rdy2;
     } iq_entry;
 
+    typedef struct arbiter {
+        unsigned int free;
+        unsigned int ready;
+        int start_index; // inclusive
+        int end_index; // exclusive
+        struct arbiter* children[ARBITER_WIDTH];
+    } arbiter;
+
 private:
     iq_entry entries[NUM_ENTRIES];
+    arbiter* arbitration_head;
     long long int IQ_occupancy; // the occupancy of the IQ
-
+    int arbiter_depth;
+    
+    arbiter* initializeArbiters(int start, int end);
+    int calculateDepth(arbiter* head);
 
 public:
     IQ();
@@ -37,11 +50,16 @@ public:
      * Adds an element to the IQ
      * 
      * Parameters:
-     * 
+     *  - in_rob_idx: rob index of the instruction to insert
+     *  - in_src1: first source register number
+     *  - in_src2: second source register number
+     *  - in_rdy1: whether the first source is ready
+     *  - in_rdy2: whether the second source is ready
      * 
      * Return: false if insertion was unsuccessful
      */
-    bool insert();
+    bool insert(bool in_rob_idx[7], uint8_t in_src1, uint8_t in_src2,
+            bool in_rdy1, bool in_rdy2);
 
     /**
      * Wakes up entries when a physical register value has been produced
